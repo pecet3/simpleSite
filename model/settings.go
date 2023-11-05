70b29b8c3335209c9f32d1b998bb1a024c5448e1
pecet3 <pecet3107@gmail.com>
2023-11-05 03:10:34 +0100

    testing

---
+++ model/settings.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func ConnectDb() {
+	var err error
+	db, err = sql.Open("postgres", "user=user1 host=localhost port=5432 password=haslo dbname=db1 sslmode=disable")
+	if err != nil {
+		fmt.Println("error during open sql")
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("error during open sql")
+	}
+	fmt.Println(db)
+}
