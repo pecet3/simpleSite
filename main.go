6864ed4f0162bbefef9d6d92735bc305cd11273c
pecet3 <pecet3107@gmail.com>
2023-11-05 20:12:18 +0100

    working creating post

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	model "simpleSite/model"
 )
 
 func main() {
 	fmt.Println("hello")
 	model.ConnectDb()
+	err := model.CreatePost("aaa", 1)
+	if err != nil {
+		log.Println("error", err)
+	}
 }
