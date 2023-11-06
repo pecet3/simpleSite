5303990d5b1894617638b63393ca2d676a274abd
pecet3 <pecet3107@gmail.com>
2023-11-06 01:10:33 +0100

    working adding posts

--- controller/controllers.go
+++ controller/controllers.go
@@ -1,32 +1,53 @@
 package controller
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"simpleSite/model"
+	"simpleSite/view"
 )
 
-func GetPosts() {
+func refreshPosts(w http.ResponseWriter) {
 	posts, err := model.GetAllPosts()
-
 	if err != nil {
-		log.Println("error get all posts: ", err)
+		log.Fatal("error during get all tasks: ", err)
 	}
 
-	fmt.Println(posts)
+	tmp := template.Must(template.ParseFiles("./view/static/index.html"))
+
+	err = tmp.ExecuteTemplate(w, "Posts", posts)
+	if err != nil {
+		log.Fatal("error executing index.html: ", err)
+	}
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	posts, err := model.GetAllPosts()
 	if err != nil {
-		log.Fatal("error during get all tasks: ", err)
+		log.Fatal("error get all posts: ", err)
 	}
-	tmp := template.Must(template.ParseFiles("./view/static/index.html"))
 
-	err = tmp.Execute(w, posts)
+	err = view.ShowIndex(posts, w)
 	if err != nil {
-		log.Fatal("error executing index.html: ", err)
+		log.Fatal("error show index: ", err)
 	}
+}
+
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+	post := &model.Post{}
+	post.Content = r.FormValue("content")
+	post.UserId = 1
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal("error during parsing form data: ", err)
+	}
+
+	post, err = post.CreatePost()
+	if err != nil {
+		log.Fatal("error during creating task:", err)
+	}
+
+	refreshPosts(w)
 }
