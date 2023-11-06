9cc23a4e92ef35bab74da45a126e935207c3cf11
pecet3 <pecet3107@gmail.com>
2023-11-07 00:28:27 +0100

    new route

--- router/router.go
+++ router/router.go
@@ -43,6 +43,7 @@ func SetupAndRun() {
 
 	mux.HandleFunc("/", controller.Index)
 	mux.HandleFunc("/post", controller.CreatePost).Methods("POST")
+	mux.HandleFunc("/register", controller.Register).Methods("POST", "GET")
 
 	log.Fatal(http.ListenAndServe(port, mux))
 }
