7fd560818c897cb2f5cc50934ef740ba994af00c
pecet3 <pecet3107@gmail.com>
2023-11-06 01:10:50 +0100

    edited view

--- view/views.go
+++ view/views.go
@@ -1,20 +1,17 @@
 package view
 
 import (
-	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"simpleSite/model"
 )
 
-func ShowIndex(posts []model.Post, w http.ResponseWriter) {
-	fmt.Println(posts, "aaaaaaaaaaaaaa")
+func ShowIndex(posts []model.Post, w http.ResponseWriter) error {
 	tmp := template.Must(template.ParseFiles("./view/static/index.html"))
 
 	err := tmp.Execute(w, posts)
 	if err != nil {
-		log.Fatal("error executing index.html: ", err)
+		return err
 	}
-
+	return nil
 }
